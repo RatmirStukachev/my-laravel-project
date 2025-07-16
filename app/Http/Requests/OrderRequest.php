68d4f2c133c88a9da2921d3eefbf0241b4d0a2cb
devilandcode <km@zmitroc.by>
2025-07-16 10:59:15 +0300

    Enhance OrderRequest validation rules and messages
    This commit updates the OrderRequest class to include comprehensive validation rules for order submissions. New rules are added for delivery and payment types, ensuring that the selected options exist in the database. Additionally, conditional rules for address fields are implemented based on the delivery type, improving the accuracy of user input. Custom validation messages are also provided in Russian for better user guidance during the order process.

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -2,14 +2,81 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Delivery;
+use App\Models\PaymentType;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OrderRequest extends FormRequest
 {
     public function rules(): array
     {
+        $deliveryId = (int)$this->input('delivery_type_id');
+
+        $rules = [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['nullable', 'string', 'max:255'],
+            'middle_name' => ['nullable', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'min:11','max:20'],
+            'email' => ['nullable', 'email:filter', 'max:55'],
+            'delivery_type_id' => ['required', Rule::exists(Delivery::class, 'id')],
+            'payment_type_id' => ['required', Rule::exists(PaymentType::class, 'id')],        
+            'message' => ['nullable', 'string'],
+            'agree' => ['required','accepted'],
+        ];
+
+        if ($deliveryId != Delivery::PICKUP_DELIVERY_ID) {
+            $rules['city'] = ['required', 'string', 'max:55'];
+            $rules['street'] = ['required', 'string', 'max:55'];
+            $rules['house'] = ['required', 'string', 'max:55'];
+            $rules['block'] = ['nullable', 'string', 'max:55'];
+            $rules['flat'] = ['required', 'string', 'max:55'];
+            $rules['entrance'] = ['nullable', 'string', 'max:55'];
+            $rules['floor'] = ['required', 'string', 'max:55'];
+            $rules['message'] = ['nullable', 'string', 'max:555'];
+        }
+
+        return $rules;
+    }
+
+    public function messages(): array
+    {
         return [
-            
+
+            'name.required' => 'Поле "Имя" обязательно для заполнения.',
+            'name.max' => 'Поле "Имя" не должно превышать 255 символов.',
+            'surname.max' => 'Поле "Фамилия" не должно превышать 255 символов.',
+            'middle_name.max' => 'Поле "Отчество" не должно превышать 255 символов.',
+            'phone.required' => 'Поле "Телефон" обязательно для заполнения.',
+            'phone.min' => 'Поле "Телефон" должно быть не менее 11 символов.',
+            'phone.max' => 'Поле "Телефон" не должно превышать 20 символов.',
+            'email.email' => 'Поле "Email" должно быть валидным email адресом.',
+            'agree.accepted' => 'Вы должны согласиться с условиями использования.',
+
+
+            'city.required' => 'Поле "Город" обязательно для заполнения.',
+            'city.string' => 'Поле "Город" должно быть строкой.',
+            'city.max' => 'Поле "Город" не должно превышать 255 символов.',
+
+            'street.required' => 'Поле "Улица" обязательно для заполнения.',
+            'street.string' => 'Поле "Улица" должно быть строкой.',
+            'street.max' => 'Поле "Улица" не должно превышать 255 символов.',
+
+            'house.required' => 'Поле "Дом" обязательно для заполнения.',
+            'house.string' => 'Поле "Дом" должно быть строкой.',
+            'house.max' => 'Поле "Дом" не должно превышать 255 символов.',
+
+            'block.string' => 'Поле "Корпус" должно быть строкой.',
+            'block.max' => 'Поле "Корпус" не должно превышать 255 символов.',
+
+            'flat.string' => 'Поле "Квартира" должно быть строкой.',
+            'flat.max' => 'Поле "Квартира" не должно превышать 255 символов.',
+
+            'entrance.string' => 'Поле "Подъезд" должно быть строкой.',
+            'entrance.max' => 'Поле "Подъезд" не должно превышать 255 символов.',
+
+            'floor.string' => 'Поле "Этаж" должно быть строкой.',
+            'floor.max' => 'Поле "Этаж" не должно превышать 255 символов.',
         ];
     }
 }
