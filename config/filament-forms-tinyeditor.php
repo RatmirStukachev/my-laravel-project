bd2399139d0a55856fdd68f224f0f331ae43abc6
devilandcode <km@zmitroc.by>
2025-07-17 16:21:03 +0300

    Refactor TinyEditor configuration to streamline profiles and templates
    This commit simplifies the TinyEditor configuration by removing unnecessary skin definitions and restructuring the profiles. The 'minimal' and 'full' profiles have been removed, while a new 'template' profile has been added. Additionally, the 'templates' section is now uncommented and properly structured to include an example template, enhancing clarity and usability of the configuration.

--- config/filament-forms-tinyeditor.php
+++ config/filament-forms-tinyeditor.php
@@ -10,19 +10,11 @@ return [
     |
     */
 
-    'skins' => [
-        // oxide, oxide-dark, tinymce-5, tinymce-5-dark
-        'ui' => 'oxide',
-
-        // dark, default, document, tinymce-5, tinymce-5-dark, writer
-        'content' => 'default'
-    ],
-
     'profiles' => [
 
         'default' => [
-            'plugins' => 'advlist autoresize codesample directionality emoticons fullscreen hr image imagetools link lists media table toc wordcount',
-            'toolbar' => 'undo redo removeformat | formatselect fontsizeselect | bold italic | rtl ltr | alignjustify alignright aligncenter alignleft | numlist bullist | forecolor backcolor | blockquote table toc hr | image link media codesample emoticons | wordcount fullscreen',
+            'plugins' => 'advlist autoresize codesample directionality emoticons fullscreen hr image imagetools link lists media table toc wordcount code',
+            'toolbar' => 'undo redo removeformat | formatselect fontsizeselect | bold italic | rtl ltr | alignjustify alignright aligncenter alignleft | numlist bullist | forecolor backcolor | blockquote table toc hr | image link media codesample emoticons | wordcount fullscreen | code',
             'upload_directory' => null,
         ],
 
@@ -32,15 +24,9 @@ return [
             'upload_directory' => null,
         ],
 
-        'minimal' => [
-                    'plugins' => 'link wordcount',
-                    'toolbar' => 'bold italic link numlist bullist',
-                    'upload_directory' => null,
-                ],
-
-        'full' => [
-            'plugins' => 'accordion autoresize codesample directionality advlist autolink link image lists charmap preview anchor pagebreak searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media table emoticons help',
-            'toolbar' => 'undo redo removeformat | fontfamily fontsize fontsizeinput font_size_formats styles | bold italic underline | rtl ltr | alignjustify alignright aligncenter alignleft | numlist bullist outdent indent accordion | forecolor backcolor | blockquote table toc hr | image link anchor media codesample emoticons | visualblocks print preview wordcount fullscreen help',
+        'template' => [
+            'plugins' => 'autoresize template',
+            'toolbar' => 'template',
             'upload_directory' => null,
         ],
         /*
@@ -90,20 +76,14 @@ return [
     | ex: TinyEditor::make('content')->profiles('template')->template('example')
     */
 
-    // 'templates' => [
+    'templates' => [
 
-    //     'example' => [
-    //         // content
-    //         ['title' => 'Some title 1', 'description' => 'Some desc 1', 'content' => 'My content'],
-    //         // url
-    //         ['title' => 'Some title 2', 'description' => 'Some desc 2', 'url' => 'http://localhost'],
-    //     ],
+        'example' => [
+            // content
+            ['title' => 'Some title 1', 'description' => 'Some desc 1', 'content' => 'My content'],
+            // url
+            ['title' => 'Some title 2', 'description' => 'Some desc 2', 'url' => 'http://localhost'],
+        ],
 
-    // ],
-    'extra' => [
-        'toolbar' => [
-            // 'fontsize' => '10px 12px 13px 14px 16px 18px 20px',
-            // 'fontfamily' => 'Tahoma=tahoma,arial,helvetica,sans-serif;',
-        ]
-    ]
+    ],
 ];
