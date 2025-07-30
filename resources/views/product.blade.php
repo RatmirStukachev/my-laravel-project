f403f77c03958ade1bb5b9168ce4ed629a0e77b4
devilandcode <km@zmitroc.by>
2025-07-30 12:32:17 +0300

    Update product image source logic for improved consistency in rendering
    This commit modifies the product view to ensure that the correct image source is used for both main and thumbnail images. The changes replace references to `$slide_block['image']` with `$image['image']`, enhancing the consistency of image rendering across different display sizes and improving overall asset management.

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -30,7 +30,7 @@
                                                 <img src="{{(new zImage($image, [435, 435], ['contain']))->resize()}}" alt="{{ $page->title }}" title="{{ $page->title }}" class="img block" @if (! $loop->first) loading="lazy" @endif>
                                                 @if(env('WEBP'))
                                                     <source
-                                                        srcset="{{(new zImage($slide_block['image'], [435, 435], ['contain'], true))->resize()}}">
+                                                        srcset="{{(new zImage($image['image'], [435, 435], ['contain'], true))->resize()}}">
                                                 @endif
                                             </picture>
                                         </a>
@@ -48,7 +48,7 @@
                                                     <img src="{{(new zImage($image, [71, 71], ['contain']))->resize()}}" alt="{{ $page->title }}" title="{{ $page->title }}" class="img block" @if (! $loop->first) loading="lazy" @endif/>
                                                     @if(env('WEBP'))
                                                         <source
-                                                            srcset="{{(new zImage($slide_block['image'], [71, 71], ['contain'], true))->resize()}}">
+                                                            srcset="{{(new zImage($image['image'], [71, 71], ['contain'], true))->resize()}}">
                                                     @endif
                                                 </picture>
                                             </div>
