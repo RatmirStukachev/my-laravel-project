8a510cb26c6d5aba6be121e6850428e46c8fff86
devilandcode <km@zmitroc.by>
2025-08-18 13:20:22 +0300

    Add redirect after product save in EditProduct page
    This commit introduces a new method `afterSave` in the `EditProduct` page, which redirects the user back to the edit page of the product after a successful save. This enhancement improves user experience by ensuring that users remain on the relevant page after making changes to a product.

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -16,4 +16,9 @@ class EditProduct extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function afterSave(): void
+    {
+        $this->redirect($this->getResource()::getUrl('edit', ['record' => $this->getRecord()]));
+    }
 }
